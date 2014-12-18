f7f0dd88998e52be232e2c46b185082d3517a851
John Stovin <john.d.stovin@gmail.com>
2014-12-18 20:54:26 +0000

    Merge

--- Config.py
+++ Config.py
@@ -1,9 +1,10 @@
 import datetime
 
-#Url = 'http://192.168.0.4'
-#BuildType = 'HelloWorld_Build'
-Url = 'http://4g5965j-dt2-s:88'
-BuildType = 'ClientExams_Build'
+Url = 'http://192.168.0.4'
+BuildType = 'HelloWorld_Build'
+#Url = 'http://4g5965j-dt2-s:88'
+#BuildType = 'ClientExams_Build'
 SleepSeconds = 10
 StartTime = datetime.time(8, 00)
-StopTime = datetime.time(18, 00)
+#StopTime = datetime.time(18, 00)
+StopTime = datetime.time(23, 59)
